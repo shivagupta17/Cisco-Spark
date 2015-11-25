0cc85a4b9c010d22acbcbc9889b66e6bef49b671
drkchiloll <samwomack_@me.com>
2015-11-25 08:09:31 -0600

    incorporate BootStrap CSS into the project

--- app/main.js
+++ app/main.js
@@ -5,6 +5,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 (() => {
+  //Incorporate BootStrap into the Index.HTML
+  const bsStyleLink = document.createElement('link');
+  const bsCDN =
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+  bsStyleLink.setAttribute('href', bsCDN);
+  bsStyleLink.setAttribute('rel', 'stylesheet');
+  document.head.appendChild(bsStyleLink);
+
+  //Add React Component
   const app = document.createElement('div');
   document.body.appendChild(app);
   ReactDOM.render(<App/>, app);
